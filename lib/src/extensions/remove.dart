4858f7eee7a7d7604f9a82f7ba22449fc4f601c9
RJM <rjmbowie@gmail.com>
2021-06-26 02:25:36 +1000

    removing debug helpers

--- lib/src/extensions/remove.dart
+++ lib/src/extensions/remove.dart
@@ -52,8 +52,6 @@ extension RemoveX<P extends IPoint> on Quadtree<P> {
         }
       }
 
-    if (node is IInternalNode<P>) print('1: ${node.nodes}');
-
     /// Find the point to remove.
     while ((node as ILeafNode<P>).point != point) {
       previous = node;
@@ -62,24 +60,17 @@ extension RemoveX<P extends IPoint> on Quadtree<P> {
 
     if ((next = node.next) != null) node.next = null;
 
-    if (node is IInternalNode<P>)
-      print('2: ${(node as IInternalNode<P>).nodes}');
-
     /// If there are multiple coincident points, remove just the point
     if (previous != null) {
       previous.next = next;
       return;
     }
-    if (node is IInternalNode<P>)
-      print('3: ${(node as IInternalNode<P>).nodes}');
 
     /// If this is the root point, remove it
     if (parent == null) {
       root = next;
       return;
     }
-    if (node is IInternalNode<P>)
-      print('4: ${(node as IInternalNode<P>).nodes}');
 
     /// Remove this leaf
     (parent.nodes ??= Nodes())[i] = next;
@@ -96,10 +87,8 @@ extension RemoveX<P extends IPoint> on Quadtree<P> {
             (node is ILeafNode<P> ||
                 (node is IInternalNode<P> && (node.nodes?.isEmpty ?? true)))) {
       if (retainer != null) {
-        print('yo: $nodes');
         retainer.nodes![j] = node;
       } else {
-        print('yo2: $node');
         root = node;
       }
     }
